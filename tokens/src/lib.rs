8bfa7eaef0c74047fefde763bbe213182042c77c
Daniel Olano <daniel@olanod.com>
2021-03-24 11:24:22 +0100

    Make tokens pallet instantiable

--- tokens/src/lib.rs
+++ tokens/src/lib.rs
@@ -155,6 +155,7 @@ pub mod module {
 		fn transfer_all() -> Weight;
 	}
 
+	// TODO: Make instantiable
 	#[pallet::config]
 	pub trait Config: frame_system::Config {
 		type Event: From<Event<Self>> + IsType<<Self as frame_system::Config>::Event>;
